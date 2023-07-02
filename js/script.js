207a14487749a05c3febfcd6970a2c1e9815e957
Ali Haider <khalihaider8@gmail.com>
2023-07-02 13:55:23 +0500

    hiding devtools

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+// document.addEventListener("keydown", function (event){
+//     if (event.ctrlKey){
+//        event.preventDefault();
+//     }
+//     if(event.key == "F12"){
+//        event.preventDefault();
+//     }
+// });
+
+// document.addEventListener('contextmenu', 
+//      event => event.preventDefault()
+// );
+
+
 // toggle icone navbar
 
 let menuIcon = document.querySelector('#menu-icon');
